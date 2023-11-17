d6dcfe81bba2abf58d67b8be76aec2b7420b2543
Suraj Prabhu <surajprabhu19@gmail.com>
2023-11-17 23:46:52 +0530

    Added Session Model

--- src/main/java/com/codergy/userservice/models/Session.java
+++ src/main/java/com/codergy/userservice/models/Session.java
@@ -1,12 +1,27 @@
 package com.codergy.userservice.models;
 
+
+import java.util.List;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
-public class Session {
+@AllArgsConstructor
+@NoArgsConstructor
+// assume one user can have only 5 active sessions at a time.
+public class Session extends BaseModel {
     private String token;
+    @ManyToOne
+    @NonNull
+    @JoinColumn(name = "user_id")
+    private User user;
 }
