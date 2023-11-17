24a1cbea9ac15f0fe609a2fd3a6c0cce2c0507b6
Suraj Prabhu <surajprabhu19@gmail.com>
2023-11-17 23:45:44 +0530

    Added User Model

--- src/main/java/com/codergy/userservice/models/User.java
+++ src/main/java/com/codergy/userservice/models/User.java
@@ -1,15 +1,34 @@
 package com.codergy.userservice.models;
 
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
-public class User {
+@AllArgsConstructor
+@NoArgsConstructor
+public class User extends BaseModel{
     private String name;
+
+    @Column(unique = true, nullable = false)
     private String email;
+    
     private String phoneNo;
+
+    @NonNull
     private String password;
+
+    @OneToMany(fetch = FetchType.EAGER, cascade = {CascadeType.ALL, CascadeType.REMOVE}, mappedBy = "user")
+    private List<Session> session;
 }
