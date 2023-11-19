ca203edbc9e136a906b959fb943cb8474224f67b
Suraj Prabhu <surajprabhu19@gmail.com>
2023-11-19 18:22:25 +0530

    Added User Controller

--- src/main/java/com/codergy/userservice/controllers/UserController.java
+++ src/main/java/com/codergy/userservice/controllers/UserController.java
@@ -3,6 +3,7 @@ package com.codergy.userservice.controllers;
 import java.util.List;
 import java.util.UUID;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
