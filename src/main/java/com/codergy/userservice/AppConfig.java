5062ca025f44ea11874182825715273ae10d6ce3
Suraj Prabhu <surajprabhu19@gmail.com>
2023-12-06 10:18:45 +0530

    Adding AppConfig.java

--- src/main/java/com/codergy/userservice/AppConfig.java
+++ src/main/java/com/codergy/userservice/AppConfig.java
@@ -3,6 +3,7 @@ package com.codergy.userservice;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 public class AppConfig {
@@ -10,5 +11,11 @@ public class AppConfig {
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bcryptEncoder()
+    {
+        return new BCryptPasswordEncoder();
     }
 }
